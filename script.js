313232902db53fef78ce497f174e28fa688b06f4
Alif Rizalif <alif.rizalif@gmail.com>
2025-06-18 23:01:29 +0700

    Add function getHumanChoice() in script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function getComputerChoice() {
   if (randomNum == 1) return "paper";
   if (randomNum == 2) return "scissor";
 }
+
+function getHumanChoice() {
+  const inputUser = prompt(
+    "Please input your selection\nrock / paper / scissor"
+  );
+  return inputUser;
+}
