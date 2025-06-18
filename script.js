8a1af05655ab9450b676b9a9c598f51b634113cb
Alif Rizalif <alif.rizalif@gmail.com>
2025-06-18 23:06:36 +0700

    Declare the score variables in script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create function to get the computer choice
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3);
 
@@ -6,9 +7,14 @@ function getComputerChoice() {
   if (randomNum == 2) return "scissor";
 }
 
+// Create function to get the human choice
 function getHumanChoice() {
   const inputUser = prompt(
     "Please input your selection\nrock / paper / scissor"
   );
   return inputUser;
 }
+
+// Declare the score variables
+let humanScore = 0;
+let computerScore = 0;
